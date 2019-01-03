6d6d159f60e856e1f9579d101e12024cee87eccf
optimus <optimus.prime.ceids@gmail.com>
2019-01-03 10:40:34 -0500

    Update step1-foreground.sh

--- hello-world/step1-foreground.sh
+++ hello-world/step1-foreground.sh
@@ -1,4 +1,6 @@
+echo "************************************************"
 echo "Configurando el entorno de manera automatica ..."
+echo "************************************************"
 apt-get update
 apt --assume-yes install ruby
 ruby --version
